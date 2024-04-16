ddbc997960a056e2975e266f6f6232212ceea795
Samuel Jacobs <Jacobssamuel57@gmail.com>
2024-04-16 10:17:30 +0100

    fixed the background and installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 
 import './App.css';
-import Header from './components/Header';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
 import Home from './pages/Home';
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import iconmoon from '../assets/iconmoon.svg';
-import iconsun from '../assets/iconsun.svg';
-import useToggle from '../hooks/useToggle';
 
 function Header({isDarkMode, toggle, icon}) {
-  // const { value: isDarkMode, toggle: darkToggle } = useToggle(false);
-
-  // let icon = isDarkMode ? iconsun : iconmoon;
  
   return (
     <div className={`wrapper ${isDarkMode ? `headerbg-dark` :  `headerbg-light`} `}>
