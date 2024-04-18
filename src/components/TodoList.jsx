b9fd9a462f52ccc8fc92742232bdbdb83cf0f06a
Samuel Jacobs <Jacobssamuel57@gmail.com>
2024-04-18 23:05:30 +0100

    more css

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,17 +3,21 @@ import '../Component.css'
 
 function TodoList({isDarkMode}) {
   return (
-    <div className='todo-list-wrapper'>
-      <ul className={`todo-list ${isDarkMode ? `dark-list` : `light-list` }`}>
+    <div className={`todo-list-wrapper ${isDarkMode ? `dark-list` : `light-list` }`}>
+      <ul className='todo-list'>
         <li>buy groceries</li>
         <li>smoke some weed</li>
         <li>have a threesome</li>
         <li>watch a football match</li>
       </ul>
-      <div>
-        <p>All</p>
-        <p>Active</p>
-        <p>Completed</p>
+      <div className='todo-settings'>
+        <p>5 minutes</p>
+        <div className='todo-category'>
+          <p>All</p>
+          <p>Active</p>
+          <p>Completed</p>
+        </div>
+        <p>Clear completed</p>
       </div>
     </div>
   )
