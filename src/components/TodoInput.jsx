c9e53f1d2eae1a6d6be6de2913f421c37671ac95
Samuel Jacobs <Jacobssamuel57@gmail.com>
2024-05-08 22:13:32 +0100

    new updates

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../Component.css';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 function TodoInput({isDarkMode}) {
+  const [input, setInput] = useState("")
   return (
     <Form className="form-wrapper">
-      <Form.Control type="text" className={`todo-input ${isDarkMode ? `dark-input` : `light-input` }`} required />
+      <Form.Control type="text" className={`todo-input ${isDarkMode ? `dark-input` : `light-input` }`} required  
+      value={input}
+      onChange={(e) => setInput(e.target.value)}/>
+ 
     </Form>
+   
   )
 }
 
